5c1ead74fe76e6291586503e70c7776a2c0bacd9
Dadadon <davianberoni@yahoo.com>
2022-08-30 11:58:03 -0400

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ function component() {
 
   // Lodash, currently included via a script, is required for this line to work
   // Lodash, now imported by this script
-    // element.innerHTML = _.join(["Hello", "webpack"], " ");
-    // element.classList.add("hello");
+  // element.innerHTML = _.join(["Hello", "webpack"], " ");
+  // element.classList.add("hello");
   return element;
 }
 
