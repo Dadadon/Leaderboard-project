5beba908198f4d69dac0c6f680bebcf6948a7469
Dadadon <davianberoni@yahoo.com>
2022-09-02 11:35:10 -0400

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,33 @@
 import _ from "lodash";
 import "./style.css";
+import grabber from "./modules/grabber.js";
+import addScore from "./modules/update.js";
+import reload from "./modules/ui";
 
-function component() {
-  const element = document.createElement("div");
+const form = document.forms["leaderboardForm"];
+const apiUrl =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/WU0ylLObsEcjpaUaHSJ4/scores";
 
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  // element.innerHTML = _.join(["Hello", "webpack"], " ");
-  // element.classList.add("hello");
-  return element;
-}
+grabber("submit").addEventListener("click", () => {
+  console.log("pressed");
+  if (form.userName.value === '') {
+    alert("Please add text");
+  } else {
+    const userName = form.userName.value;
+    const score = form.score.value;
 
-document.body.appendChild(component());
+    const data = {
+      user: userName,
+      score: score,
+    };
+    addScore(apiUrl, data);
+    reload(apiUrl);
+    
+  }
+});
+
+grabber("refresh").addEventListener("click", () => {
+  reload(apiUrl);
+});
+
+reload(apiUrl);
